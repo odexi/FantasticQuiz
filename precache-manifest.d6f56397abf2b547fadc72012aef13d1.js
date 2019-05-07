e0f8f33d05666f0b5a72a6e64394242e231ee1c2
odexi <otakar93@gmail.com>
2019-05-07 16:25:57 +0300

    Updates

---
+++ precache-manifest.d6f56397abf2b547fadc72012aef13d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "628470f5275b1ff6c99a22d89a768b6e",
+    "url": "/FantasticQuiz/index.html"
+  },
+  {
+    "revision": "c58021de2cb795cab541",
+    "url": "/FantasticQuiz/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "9e6c63d58b91521030a2",
+    "url": "/FantasticQuiz/static/js/2.1f13c71f.chunk.js"
+  },
+  {
+    "revision": "c58021de2cb795cab541",
+    "url": "/FantasticQuiz/static/js/main.e867afc7.chunk.js"
+  },
+  {
+    "revision": "2ae3bd97773135787ea7",
+    "url": "/FantasticQuiz/static/js/runtime~main.5493f255.js"
+  }
+]);
